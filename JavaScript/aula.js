83d155453b08b1a5995a8de87ec515f7f0a16baa
Vinícius Barros <Vrdb.eai21@uea.edu.br>
2023-10-30 20:20:13 -0400

    Início da aula de JavaScript

--- JavaScript/aula.js
+++ JavaScript/aula.js
@@ -1,5 +1,7 @@
-function fn2(){
-    console.log("Olá mundo");
+/*function fn2(){
+    setTimeout(() => {
+        console.log("oi");
+    });
 }
 
 function fn1(){
@@ -12,3 +14,61 @@ function main () {
 
 main();
 console.log('Tchau mundo')
+
+
+var nome = "Livía";
+console.log(nome);
+
+nome = '22';
+console.log(nome);
+
+console.log(nome + 10);
+
+console.log('10' + '2'); //haverá uma concatenação de string
+
+console.log('10' / '2'); // o JavaScript interpreta as strings como números e realiza a operação
+
+existem 3 formas para atribuir variáveis: o var, let e const:
+
+
+let valor3 = 'ABACATE';
+
+function main(){
+    valor3 = 'LARANJO'
+    console.log(valor3);
+}
+main();
+console.log(valor3);
+*/
+
+const euclido = 1;
+
+function main() {
+    // // if(euclido >2 && euclido < 5) {
+    // //     console.log('TA ERRADO');
+    // // }
+
+    // //para comentar várias linhas de uma vez a gente seleciona, clica ctrl k + ctrl c
+
+    // const numero = 1;
+    // numero++;
+    // numer' '
+
+    // && -> e
+    // || -> ou 
+
+    // ! -> negação
+
+    // = -> atribuição
+    // == -> verifica a tipagem
+    // === -> verifica a tipagem e a referência
+
+
+    const verdadeiro = 'banana';
+    if (!verdadeiro == 'banana'){
+        console.log('VERDADEIRO');
+    } else {
+        console.log('FALSO');
+    }
+}
+    main();
