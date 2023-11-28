92eaec354c4ed5ae47be2c6e0317fcaa1c7fc852
Vinícius Barros <Vrdb.eai21@uea.edu.br>
2023-11-27 20:19:24 -0400

    Continuação do curso de angular

--- Angular/heroes/src/app/hero.service.ts
+++ Angular/heroes/src/app/hero.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './heroes/mocks/hero.mocks';
 import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  constructor(private messageService: MessageService){}
 
-  getHeroes():Hero[] {
-    return HEROES
+  getHeroesMock():Observable<Hero[]> {
+    const heroes = of (HEROES);
+    this.messageService.add("Hero Service: fetched heroes");
+    return heroes
   }
 }
