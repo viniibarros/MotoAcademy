880bcb12c6d85f5314cefea32cbf4501329af775
Vinícius Barros <Vrdb.eai21@uea.edu.br>
2023-12-01 20:26:04 -0400

    criação de mais um servidor pra começar a trabalhar com rotas http

--- Angular/heroes/src/app/app.module.ts
+++ Angular/heroes/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -17,11 +19,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MessagesComponent,
     DashboardComponent
   ],
+
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
